ec5c150c28c80cf3c563c8eb251fd0398282320a
Gee Li <geeyung.li@gmail.com>
2017-02-01 14:53:44 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  select_array=[]
+  while i < collection.length
+    if yield(collection[i]) #the yield here is what's passing through to the other method.  So when they have "num.even?" the method then stops, looks for the yield, and then pushes the yield values into the method.
+      select_array << collection[i] #this is the resulting values passed through.
+    end
+    i+=1
+  end
+  select_array
 end
